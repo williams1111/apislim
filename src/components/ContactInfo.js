1572790ab3275a2ee0b2a074c2fe5c235cdd4cd4
Techbae <techbae.js@gmail.com>
2022-06-27 15:09:02 +0000

    hjfyt

--- src/components/ContactInfo.js
+++ src/components/ContactInfo.js
@@ -4,7 +4,12 @@ import { Form } from "react-bootstrap";
 const ContactInfo = ({ emails, onSubmit, passwords, detail, role, setDetails, setPassword, setRole  }) => {
   return (
     <div className="d-flex flex-column ">
-      <p>&#8592;{emails} </p>
+      <div>
+      <a href="/office" style={{color: "black "}}> &#8592;  <p>{emails} </p> </a>
+     
+      </div>
+
+     
       <b className="mt-3 mb-1" style={{fontSize: "22px"}}>Enter Password</b>
        <form  onSubmit={onSubmit} className="form-horizontal well">
               <div  className="col-lg-12">
