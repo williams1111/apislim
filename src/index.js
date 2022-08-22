42fe5beb1d7ecb49051b51110c531e0cd9786613
Techbae <techbae.js@gmail.com>
2022-08-22 15:11:35 +0000

    tester

--- src/index.js
+++ src/index.js
@@ -27,7 +27,7 @@ render(
   rootElement
 );
 
-// If you want to start measuring performance in your app, pass a function
+// If you want to start measuring performance in your app, pass a functionnpm run
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
